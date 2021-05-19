e3fea3927b269f252679629ee82c4b3fa9b1ab61
Donna1309 <62295055+Donna1309@users.noreply.github.com>
2021-05-19 17:27:50 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,9 @@
+function purple_button(){
+document.body.style.background="purple";
+}
+function rosybrown_button() {
+    document.body.style.background="rosybrown";
+}
+function  teal_button(){
+    document.body.style.background="teal";
+}
